4cc58870e4233e7bf7f096e4b7ae08a01c1a3f93
Konstantin <31697707+konceto@users.noreply.github.com>
2022-11-20 13:53:28 +0200

    Update client.py

--- client.py
+++ client.py
@@ -10,14 +10,24 @@ player_name = input(" -> ")
 
 s.connect((host, port))
 s.send(player_name.encode(f))
+print("Waiting for opponent to join...")
+msg = s.recv(1024).decode(f)
+print(msg)
 
 
 while True:
-    print("Enter: Rock(r) Paper(p) Scissors(s)")
+
+    print("\nEnter: Rock(r) Paper(p) Scissors(s) Exit(close)")
     s_msg = input(" -> ")
+    while True:
+        if s_msg == 'r' or s_msg == 's' or s_msg == 'p' or s_msg == 'close':
+            break
+        s_msg = input(" -> ")
     if s_msg == 'close':
+        s.send(s_msg.encode(f))
         break
     s.send(s_msg.encode(f))
+    print(f"Waiting for opponent to play...")
     r_msg = s.recv(1024).decode(f)
     print(r_msg)
 
