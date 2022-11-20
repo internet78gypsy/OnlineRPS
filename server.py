7b0e40278e086b2879c085c5fdae0e255181ff37
Konstantin <31697707+konceto@users.noreply.github.com>
2022-11-20 13:54:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ host = '192.168.170.109'
 port = 5000
 f = 'utf-8'
 players = ['', '']
+names = ['', '']
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -12,44 +14,57 @@ s.bind((host, port))
 
 def cope_with_client(con_socket, addr):
     name = con_socket.recv(1024).decode(f)
-    print(f"Player {name} connected the server")
     num = int(threading.current_thread().name[7:8]) - 1
+    names[num] = name
+    print(f"Connection from {addr}")
+    print(f"{names[num]} joined the game")
+    while True:
+        if names[abs(num - 1)] != '':
+            break
+    msg = f"{names[abs(num - 1)]} joined the game"
+    con_socket.send(msg.encode(f))
+
     while True:
         r_msg = con_socket.recv(1024).decode(f)
         if r_msg == "close":
             break
-        print(f"Received: {r_msg}")
+        print(f"{names[num]} played: {r_msg}")
         players[num] = r_msg
-        print(players)
         wait = True
+        result = ''
         while wait:
             if players[0] != '' and players[1] != '':
                 wait = False
         if players[0] == players[1]:
-            result = 'Tie!'
+            result = f'{names[abs(num - 1)]} played {players[abs(num - 1)]}. \nTie.'
         elif (players[0] == 'r' and players[1] == 's') or (players[0] == 'p' and players[1] == 'r') or (
                 players[0] == 's' and players[1] == 'p'):
             if num == 0:
-                result = 'You win!'
+                result = f'{names[abs(num - 1)]} played {players[abs(num - 1)]}. \nYou win!'
             else:
-                result = 'You lose.'
+                result = f'{names[abs(num - 1)]} played {players[abs(num - 1)]}. \nYou lose.'
         elif (players[0] == 's' and players[1] == 'r') or (players[0] == 'r' and players[1] == 'p') or (
                 players[0] == 'p' and players[1] == 's'):
             if num == 0:
-                result = 'You lose.'
+                result = f'{names[abs(num - 1)]} played {players[abs(num - 1)]}. \nYou lose.'
             else:
-                result = 'You win!'
+                result = f'{names[abs(num - 1)]} played {players[abs(num - 1)]}. \nYou win!'
         con_socket.send(result.encode(f))
-    print(f"Player {name} left the game.")
+        players[0] = ''
+        players[1] = ''
+    print(f"{names[num]} left the game.")
+    names[num] = ''
     con_socket.close()
 
 
 def start_server():
-    s.listen(2)
     while True:
+        s.listen(2)
         con_socket, addr = s.accept()
         thread = threading.Thread(target=cope_with_client, args=(con_socket, addr))
         thread.start()
 
 
+print(f"Server started at: {host}")
+print("Waiting for players to join...")
 start_server()
